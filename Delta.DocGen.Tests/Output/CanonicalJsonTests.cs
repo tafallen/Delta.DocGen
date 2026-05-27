d38f634364e9079d780a02c13e17f9ee2f2fbc54
Tim Allen <tim.allen@trinitatum.com>
2026-05-27 14:55:02 +0100

    test: nested sorting, array order, no whitespace for CanonicalJson (Story 10, task 2)

--- Delta.DocGen.Tests/Output/CanonicalJsonTests.cs
+++ Delta.DocGen.Tests/Output/CanonicalJsonTests.cs
@@ -19,4 +19,45 @@ public sealed class CanonicalJsonTests
         applePos.Should().BeLessThan(mangoPos);
         mangoPos.Should().BeLessThan(zebraPos);
     }
+
+    [Fact]
+    public void NestedObjectKeysAreSorted()
+    {
+        var obj = new { outer = new { z = 1, a = 2 } };
+
+        var json = CanonicalJson.Serialise(obj);
+
+        // Inside "outer", "a" must appear before "z"
+        var outerPos = json.IndexOf("outer", StringComparison.Ordinal);
+        var aPos     = json.IndexOf("\"a\"",  StringComparison.Ordinal);
+        var zPos     = json.IndexOf("\"z\"",  StringComparison.Ordinal);
+        aPos.Should().BeGreaterThan(outerPos);
+        aPos.Should().BeLessThan(zPos);
+    }
+
+    [Fact]
+    public void ArrayElementOrderIsPreserved()
+    {
+        var obj = new { items = new[] { "charlie", "alpha", "bravo" } };
+
+        var json = CanonicalJson.Serialise(obj);
+
+        var charliePos = json.IndexOf("charlie", StringComparison.Ordinal);
+        var alphaPos   = json.IndexOf("alpha",   StringComparison.Ordinal);
+        var bravoPos   = json.IndexOf("bravo",   StringComparison.Ordinal);
+        charliePos.Should().BeLessThan(alphaPos);
+        alphaPos.Should().BeLessThan(bravoPos);
+    }
+
+    [Fact]
+    public void OutputContainsNoWhitespace()
+    {
+        var obj = new { key = "value", number = 42 };
+
+        var json = CanonicalJson.Serialise(obj);
+
+        json.Should().NotContain(" ");
+        json.Should().NotContain("\n");
+        json.Should().NotContain("\r");
+    }
 }
