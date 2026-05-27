93db12f0ea714eb0658257688fcc3f8633e560eb
Tim Allen <tim.allen@trinitatum.com>
2026-05-27 13:51:31 +0100

    test: fallback domain and warning logging for DomainAssigner (Story 8, task 2)

--- Delta.DocGen.Tests/Pipeline/DomainAssignerTests.cs
+++ Delta.DocGen.Tests/Pipeline/DomainAssignerTests.cs
@@ -23,4 +23,48 @@ public sealed class DomainAssignerTests
         result.Should().ContainSingle();
         result[0].Domain.Should().Be("Auth");
     }
+
+    [Fact]
+    public void StepMatchingNoRuleIsAssignedFallbackDomain()
+    {
+        var steps = new List<RawStep>
+        {
+            new(StepType.Given, "I do something", [], "Other/OtherSteps.cs", 1, "")
+        };
+        var rules = new List<DomainRule> { new("Auth/**", "Auth", "Auth & Identity") };
+
+        var result = DomainAssigner.Assign(steps, rules, "General", NullDocGenLogger.Instance);
+
+        result[0].Domain.Should().Be("General");
+    }
+
+    [Fact]
+    public void UnmatchedStepLogsWarning()
+    {
+        var logger = new CapturingDocGenLogger();
+        var steps = new List<RawStep>
+        {
+            new(StepType.Given, "I do something", [], "Other/OtherSteps.cs", 1, "")
+        };
+        var rules = new List<DomainRule> { new("Auth/**", "Auth", "Auth & Identity") };
+
+        DomainAssigner.Assign(steps, rules, "General", logger);
+
+        logger.WarnMessages.Should().ContainSingle(m => m.Contains("Other/OtherSteps.cs"));
+    }
+
+    [Fact]
+    public void MatchedStepDoesNotLogWarning()
+    {
+        var logger = new CapturingDocGenLogger();
+        var steps = new List<RawStep>
+        {
+            new(StepType.Given, "I am logged in", [], "Auth/AuthSteps.cs", 1, "")
+        };
+        var rules = new List<DomainRule> { new("Auth/**", "Auth", "Auth & Identity") };
+
+        DomainAssigner.Assign(steps, rules, "General", logger);
+
+        logger.WarnMessages.Should().BeEmpty();
+    }
 }
