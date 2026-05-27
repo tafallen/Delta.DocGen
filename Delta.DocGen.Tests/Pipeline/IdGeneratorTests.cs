31ea727c2305de2d3d17a65eb6f5cce307df049e
Tim Allen <tim.allen@trinitatum.com>
2026-05-27 14:46:24 +0100

    test: domain records for IdGenerator (Story 9, task 5)

--- Delta.DocGen.Tests/Pipeline/IdGeneratorTests.cs
+++ Delta.DocGen.Tests/Pipeline/IdGeneratorTests.cs
@@ -93,4 +93,45 @@ public sealed class IdGeneratorTests
 
         act.Should().Throw<InvalidOperationException>().WithMessage("*collision*");
     }
+
+    [Fact]
+    public void DomainRecordsAreDistinctInFirstOccurrenceOrder()
+    {
+        var steps = new List<RawStep>
+        {
+            new(StepType.Given, "step one",   [], "Auth/AuthSteps.cs",  1, "", "Auth"),
+            new(StepType.Given, "step two",   [], "Forms/FormSteps.cs", 1, "", "Forms"),
+            new(StepType.Given, "step three", [], "Auth/AuthSteps2.cs", 5, "", "Auth"),  // Auth again
+        };
+        var rules = new List<DomainRule>
+        {
+            new("Auth/**",  "Auth",  "Auth & Identity"),
+            new("Forms/**", "Forms", "Forms & Input"),
+        };
+
+        var (_, domains) = IdGenerator.Generate(
+            steps, new Dictionary<string, int>(), rules, "General", NullDocGenLogger.Instance);
+
+        domains.Should().HaveCount(2);
+        domains[0].Id.Should().Be("Auth");
+        domains[0].Label.Should().Be("Auth & Identity");
+        domains[1].Id.Should().Be("Forms");
+        domains[1].Label.Should().Be("Forms & Input");
+    }
+
+    [Fact]
+    public void FallbackDomainUsesItsOwnIdAsLabel()
+    {
+        var steps = new List<RawStep>
+        {
+            new(StepType.Given, "step one", [], "Other/OtherSteps.cs", 1, "", "General")
+        };
+
+        var (_, domains) = IdGenerator.Generate(
+            steps, new Dictionary<string, int>(), [], "General", NullDocGenLogger.Instance);
+
+        domains.Should().ContainSingle();
+        domains[0].Id.Should().Be("General");
+        domains[0].Label.Should().Be("General");
+    }
 }
